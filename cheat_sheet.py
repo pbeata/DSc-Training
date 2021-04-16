88e817e2b6ea102884472e4613240dc732207ec3
Paul A. Beata <pbeata@users.noreply.github.com>
2021-04-15 22:03:34 -0400

    completed KNN applications for 1. gene types and 2. sonar data

---
+++ cheat_sheet.py
@@ -0,0 +1,15 @@
+
+# Python Cheat Sheet
+# Updated: April 15, 2021
+
+# COMMON PACKAGES USED
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+%config Completer.use_jedi = False
+
+# TRAIN-TEST SPLIT TEMPLATE
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(
+...     X, y, test_size=0.10, random_state=101)
